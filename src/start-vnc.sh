b3b9488715e9c43d921adce53c5be481624a6a7a
simojenki <simojenki@users.noreply.github.com>
2022-09-27 12:27:22 +1000

    move creation of xstartup to init

--- src/start-vnc.sh
+++ src/start-vnc.sh
@@ -14,15 +14,6 @@ readonly vnc_port=$((5900+VNC_DISPLAY))
 
 echo "Running vncserver :${VNC_DISPLAY} depth=${VNC_DEPTH}, geometry=${VNC_GEOMETRY} as $(id), HOME=${HOME}"
   
-cat << END > "${HOME}/.vnc/xstartup"
-#!/bin/bash
-
-xrdb ${HOME}/.Xresources
-xsetroot -solid grey
-startlxde
-END
-chmod 700 "${HOME}/.vnc/xstartup"
-
 touch "${HOME}/.Xauthority" "${HOME}/.Xresources"
 
 vncserver \
